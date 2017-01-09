f8b7344d375e34822a28979f9c56d402153c809d
Bryan Marsh <Bryan@eduroam1-2-10-25-198-56.wnet.ucr.edu>
2017-01-09 10:10:04 -0800

    Template for Assignment 1

---
+++ assn1.asm
@@ -0,0 +1,40 @@
+;=================================================
+; Name: 
+; Email: 
+; 
+; Assignment name: Assignment 1
+; Lab section: 
+; 
+; I hereby certify that I have not received assistance on this assignment,
+; or used code, from ANY outside source other than the instruction team.
+;
+;=================================================
+
+;-------------------------------
+;PUT TABLE HERE
+;--------------------------------
+
+
+
+
+.ORIG x3000			; Program begins here
+;-------------
+;Instructions
+;-------------
+
+;-------------------------------
+;INSERT CODE STARTING FROM HERE
+;--------------------------------
+
+HALT
+;---------------	
+;Data
+;---------------
+
+
+;---------------	
+;END of PROGRAM
+;---------------	
+.END
+
+
